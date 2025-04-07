a373fb3ec9e44baae3300726bcf2a97348b69772
dannyarena <arenadanny71@gmail.com>
2025-04-07 14:49:53 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// -------- PALINDROMA --------
+// 1. Chiedere all’utente di inserire una parola
+// 2. Creare una funzione che controlla se la parola è palindroma
+// 3. Stampare il risultato: è palindroma o no
+
+// -------- PARI E DISPARI --------
+// 4. Chiedere all’utente di scegliere tra "pari" o "dispari"
+// 5. Chiedere all’utente di inserire un numero da 1 a 5
+// 6. Creare una funzione che genera un numero casuale da 1 a 5 per il computer
+// 7. Sommare il numero dell’utente con quello del computer
+// 8. Creare una funzione che controlla se la somma è pari o dispari
+// 9. Dichiarare chi ha vinto: l’utente o il computer
