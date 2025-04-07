d66415323a8fc2f3f5547a1440c8475d797a0ced
dannyarena <arenadanny71@gmail.com>
2025-04-07 15:16:12 +0200

    Add random number generation for computer's choice in even-odd game

--- pari o dispari/script.js
+++ pari o dispari/script.js
@@ -1,2 +1,14 @@
 const sceltaUtente = prompt("Scegli: pari o dispari");
 const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+
+function generaNumeroRandom() {
+    return Math.floor(Math.random() * 5) + 1;
+  }
+  
+  const numeroComputer = generaNumeroRandom();
+console.log("Il computer ha scelto:", numeroComputer);
+
+
+const somma = numeroUtente + numeroComputer;
+console.log("La somma dei numeri è:", somma);
